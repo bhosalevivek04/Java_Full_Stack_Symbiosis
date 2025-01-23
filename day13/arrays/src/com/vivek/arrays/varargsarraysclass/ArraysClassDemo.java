e610e483d807d5e153bb167a1d1390ccb8a151f3
Vivek Bhosale <135112044+bhosalevivek04@users.noreply.github.com>
2025-01-23 11:11:57 +0530

    Day 18 Arrays Class

---
+++ day13/arrays/src/com/vivek/arrays/varargsarraysclass/ArraysClassDemo.java
@@ -0,0 +1,18 @@
+package com.vivek.arrays.varargsarraysclass;
+
+import java.util.Arrays;
+
+public class ArraysClassDemo {
+	public static void main(String[] args) {
+		int[] array = new int[5];
+//		print(array);
+
+		Arrays.fill(array, 1, 4, 3);
+//		print(array);
+		print(5, "Vivek", 1, 2, 3, 4, 5);
+	}
+
+	public static void print(int length, String name, int... array) {
+		System.out.println(Arrays.toString(array));
+	}
+}
